60176c99193fa350b993a9d47d4365dbb7a37154
Parveen Mangla <parveen@recstech.com>
2017-05-13 14:56:37 +0530

    aaaaaaaaaaaa...

--- pc-entities/src/main/java/com/pc/app/PcEntitiesApplication.java
+++ pc-entities/src/main/java/com/pc/app/PcEntitiesApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //parveen
 
+
+//jjjjjjjjjjjjjj
 @SpringBootApplication
 public class PcEntitiesApplication {
 
